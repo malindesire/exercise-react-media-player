1ac6887278710d30af7d707844e71dd2ef273a10
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 16:02:19 +0200

    Add mock data and images

---
+++ media-player/src/utilities/mock.ts
@@ -0,0 +1,75 @@
+import type { Media } from './types';
+
+export const songs: Media[] = [
+	{
+		imageSrc: '/src/assets/images/the-las.jpg',
+		imageAlt: "Albumomslag för The La's",
+		creator: "The La's",
+		title: 'There She Goes',
+		length: 162,
+	},
+	{
+		imageSrc: '/src/assets/images/oasis.jpg',
+		imageAlt: 'Albumomslag för Oasis – Definitely Maybe',
+		creator: 'Oasis',
+		title: 'Live Forever',
+		length: 276,
+	},
+	{
+		imageSrc: '/src/assets/images/the-stone-roses.jpg',
+		imageAlt: 'Albumomslag för The Stone Roses',
+		creator: 'The Stone Roses',
+		title: 'I Wanna Be Adored',
+		length: 292,
+	},
+	{
+		imageSrc: '/src/assets/images/arctic-monkeys.jpg',
+		imageAlt:
+			"Albumomslag för Arctic Monkeys – Whatever People Say I Am, That's What I'm Not",
+		creator: 'Arctic Monkeys',
+		title: 'I Bet You Look Good on the Dancefloor',
+		length: 173,
+	},
+	{
+		imageSrc: '/src/assets/images/blur.jpg',
+		imageAlt: 'Albumomslag för Blur – Parklife',
+		creator: 'Blur',
+		title: 'Girls & Boys',
+		length: 258,
+	},
+	{
+		imageSrc: '/src/assets/images/the-verve.png',
+		imageAlt: 'Albumomslag för The Verve – Urban Hymns',
+		creator: 'The Verve',
+		title: 'Bitter Sweet Symphony',
+		length: 358,
+	},
+	{
+		imageSrc: '/src/assets/images/oasis2.jpeg',
+		imageAlt: "Albumomslag för Oasis – (What's the Story) Morning Glory?",
+		creator: 'Oasis',
+		title: 'Wonderwall',
+		length: 258,
+	},
+	{
+		imageSrc: '/src/assets/images/radiohead.png',
+		imageAlt: 'Albumomslag för Radiohead – OK Computer',
+		creator: 'Radiohead',
+		title: 'No Surprises',
+		length: 229,
+	},
+	{
+		imageSrc: '/src/assets/images/coldplay.png',
+		imageAlt: 'Albumomslag för Coldplay – Parachutes',
+		creator: 'Coldplay',
+		title: 'Yellow',
+		length: 269,
+	},
+	{
+		imageSrc: '/src/assets/images/suede.jpg',
+		imageAlt: 'Albumomslag för Suede – Suede',
+		creator: 'Suede',
+		title: 'Animal Nitrate',
+		length: 206,
+	},
+];

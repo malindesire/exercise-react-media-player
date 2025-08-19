9ec3c93356759840447ce4afb0dc6e72f1c0396b
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 12:39:00 +0200

    Adjust svgs

--- media-player/src/components/Icon/Play.tsx
+++ media-player/src/components/Icon/Play.tsx
@@ -4,9 +4,9 @@ export const Play = () => {
 			xmlns="http://www.w3.org/2000/svg"
 			width="512"
 			height="512"
-			viewBox="0 0 8 8"
+			viewBox="0 0 24 24"
 		>
-			<path fill="currentColor" d="M1 1v6l6-3l-6-3z" />
+			<path fill="currentColor" d="M8 19V5l11 7l-11 7Z" />
 		</svg>
 	);
 };
--- media-player/src/components/Icon/Previous.tsx
+++ media-player/src/components/Icon/Previous.tsx
@@ -4,9 +4,12 @@ export const Previous = () => {
 			xmlns="http://www.w3.org/2000/svg"
 			width="512"
 			height="512"
-			viewBox="0 0 8 8"
+			viewBox="0 0 20 20"
 		>
-			<path fill="currentColor" d="M4 1L0 4l4 3V1zm0 3l4 3V1L4 4z" />
+			<path
+				fill="currentColor"
+				d="m10.903 2.931l-.001 5.108l6.615-5.593c.62-.526 1.58-.323 1.58.485v14.14c0 .805-.96 1.009-1.58.483l-6.615-5.593v5.11c0 .805-.96 1.009-1.58.483l-8.085-6.836a.936.936 0 0 1 0-1.434l8.086-6.838c.62-.526 1.58-.323 1.58.485Z"
+			/>
 		</svg>
 	);
 };

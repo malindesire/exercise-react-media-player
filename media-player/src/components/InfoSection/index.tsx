c35eaa79577fcb76a720be0e5e0cfeec9869b56d
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 16:34:33 +0200

    Add InfoSection component

---
+++ media-player/src/components/InfoSection/index.tsx
@@ -0,0 +1,27 @@
+import { IconButton } from '../IconButton';
+import { Info } from '../Info';
+import styles from './style.module.css';
+
+type InfoSectionProps = {
+	creator: string;
+	title: string;
+	favorite?: boolean;
+};
+
+export const InfoSection = ({
+	creator,
+	title,
+	favorite = false,
+}: InfoSectionProps) => {
+	return (
+		<div className={styles.section}>
+			<IconButton icon="add" className={styles.button} />
+			<Info creator={creator} title={title} />
+			<IconButton
+				icon="heart"
+				className={styles.button}
+				active={favorite}
+			/>
+		</div>
+	);
+};

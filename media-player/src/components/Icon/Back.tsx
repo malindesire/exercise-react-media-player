b12da0b212755dc06c434a2c127af1d7b51f1329
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 17:33:38 +0200

    Use typescript attributes

--- media-player/src/components/Icon/Back.tsx
+++ media-player/src/components/Icon/Back.tsx
@@ -8,7 +8,7 @@ export const Back = () => {
 		>
 			<path
 				fill="currentColor"
-				fill-rule="evenodd"
+				fillRule="evenodd"
 				d="m3.343 12l7.071 7.071L9 20.485l-7.778-7.778a1 1 0 0 1 0-1.414L9 3.515l1.414 1.414z"
 			/>
 		</svg>

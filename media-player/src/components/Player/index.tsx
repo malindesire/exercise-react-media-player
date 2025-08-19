688d06c5c6c1cfb5cc5f7dbe2c6632fb523e0217
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 18:09:44 +0200

    Add Player component

---
+++ media-player/src/components/Player/index.tsx
@@ -0,0 +1,24 @@
+import type { Media } from '../../utilities/types';
+import { ButtonSection } from '../ButtonSection';
+import { Image } from '../Image';
+import { InfoSection } from '../InfoSection';
+import { Progress } from '../Progress';
+import styles from './style.module.css';
+
+type PlayerProps = {
+	media: Media;
+	progress: number;
+};
+
+export const Player = ({ media, progress }: PlayerProps) => {
+	const { imageSrc, imageAlt, creator, title, length } = media;
+
+	return (
+		<section className={styles.section}>
+			<Image src={imageSrc} alt={imageAlt} />
+			<InfoSection creator={creator} title={title} />
+			<Progress progress={progress} totalLength={length} />
+			<ButtonSection />
+		</section>
+	);
+};

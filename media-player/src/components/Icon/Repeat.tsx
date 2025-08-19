306fc2596cdd0e8d03b0b2fb0732cb7d4524c889
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 18:10:54 +0200

    Use typescript attributes

--- media-player/src/components/Icon/Repeat.tsx
+++ media-player/src/components/Icon/Repeat.tsx
@@ -6,7 +6,7 @@ export const Repeat = () => {
 			height="512"
 			viewBox="0 0 20 20"
 		>
-			<g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd">
+			<g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
 				<path d="M14.519 2.749a.75.75 0 0 1 1.052.13l1.547 1.982a.75.75 0 1 1-1.183.923L14.39 3.8a.75.75 0 0 1 .13-1.052Z" />
 				<path d="M16.983 4.727a.75.75 0 0 0-1.052.14l-1.546 2.017a.75.75 0 1 0 1.19.912l1.547-2.017a.75.75 0 0 0-.14-1.052Z" />
 				<path d="M2.48 9.323a5 5 0 0 1 5-5h7.86a1 1 0 1 1 0 2H7.48a3 3 0 0 0-3 3v1a1 1 0 1 1-2 0v-1Zm3.008 7.928a.75.75 0 0 1-1.053-.13L2.89 15.14a.75.75 0 1 1 1.182-.923L5.619 16.2a.75.75 0 0 1-.13 1.052Z" />

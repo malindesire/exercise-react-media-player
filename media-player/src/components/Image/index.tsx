9576eb6550d29b1130884521a29e1ae19fcf765a
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-19 13:56:22 +0200

    Add container to Image component

--- media-player/src/components/Image/index.tsx
+++ media-player/src/components/Image/index.tsx
@@ -1,10 +1,16 @@
+import clsx from 'clsx';
 import styles from './style.module.css';
 
 type ImageProps = {
 	src: string;
 	alt: string;
+	className?: string;
 };
 
-export const Image = ({ src, alt }: ImageProps) => {
-	return <img className={styles.image} src={src} alt={alt} />;
+export const Image = ({ src, alt, className }: ImageProps) => {
+	return (
+		<div className={clsx(styles.image, className)}>
+			<img src={src} alt={alt} />
+		</div>
+	);
 };

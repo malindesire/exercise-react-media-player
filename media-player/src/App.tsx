ea7ffd222ad9125b710334f448e88b16531cd0fd
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-08-20 16:13:30 +0200

    Implement Header and Main in App

--- media-player/src/App.tsx
+++ media-player/src/App.tsx
@@ -1,5 +1,14 @@
+import { Header } from './components/Header';
+import { songs } from './utilities/mock';
+import { Main } from './components/Main';
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Header />
+			<Main media={songs} />
+		</>
+	);
 }
 
 export default App;
